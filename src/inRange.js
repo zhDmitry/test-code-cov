06963815fe78fedfbcaabd74fbd35717b979a87d
Dmitry Zherebko <kraken@live.ru>
2018-12-20 17:05:56 +0200

    Update inRange.js

--- src/inRange.js
+++ src/inRange.js
@@ -1,5 +1,8 @@
 function inRange({ number, low, high }) {
   return number >= low && number <= high;
 }
+function inRange2({ number, low, high }) {
+  return number >= low && number <= high;
+}
 
 export default inRange;
